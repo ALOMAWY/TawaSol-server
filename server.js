d84da2c11198112c64fe12d23f8f3403fcd9182d
Abdalrahman Aldabbas <aldabbas333abdalrahman@gmail.com>
2024-12-30 01:54:22 +0300

    initial commit

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require("express");
 
+const connectDB = require("./config/db");
+
 const app = express();
+
+const cors = require("cors");
+
+// To Parse JSON File From Express
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.send(`Server Is Working Correctly`);
 });
 
+app.use("/api/users", require("./routes/users"));
+app.use("/api/profiles", require("./routes/profiles"));
+app.use("/api/posts", require("./routes/posts"));
+
 const PORT = process.env.PORT || 4000;
 
+// Start Application
 app.listen(PORT, () => {
   console.log(`Server Started On PORT : ${PORT}`);
 });
---
+++ utils/index.js
@@ -0,0 +1,52 @@
+const JWT = require("jsonwebtoken");
+
+const config = require("config");
+
+const multer = require("multer");
+const { console } = require("inspector");
+
+const auth = (req, res, next) => {
+  // Get The Token From The Requset Header
+
+  const token = req.header("x-auth-token");
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ msg: "Token Is Not Available, Authorization Denied." });
+  }
+
+  try {
+    JWT.verify(token, config.get("jwtSecret"), (error, decoded) => {
+      if (error) {
+        return res
+          .status(401)
+          .json({ msg: "Token Is Not Valid, Authorization Denied." });
+      } else {
+        req.user = decoded.user;
+        next();
+      }
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: err.message });
+  }
+};
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${req.user.id}`);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 2 * 1024 * 1024,
+  },
+}).single("file");
+
+module.exports = { auth, upload };
